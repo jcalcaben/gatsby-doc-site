ce66e54604a40c7c6d78a9e5b2acd097fbb5b159
jcalcaben <jcalcaben@gmail.com>
2019-12-11 02:36:59 +0000

    jcalcaben published a site update

---
+++ component---src-pages-page-2-js-e2ceb3d3c7a498b10cea.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("Wbzz"),l=a("POju");t.default=function(){return o.a.createElement(l.a,{title:"Page two",slug:"/page-2/"},o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-e2ceb3d3c7a498b10cea.js.map
