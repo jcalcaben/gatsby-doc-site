7a4e6ee553af1cd826a136629a691a3bc6493a99
jcalcaben <jcalcaben@gmail.com>
2019-12-18 03:09:59 +0000

    jcalcaben published a site update

--- component---src-pages-404-js-a78451af7e166639a711.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),s=n("POju");e.default=function(){return u.a.createElement(s.a,{title:"404: Not found",slug:"/404/"},u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-a78451af7e166639a711.js.map
