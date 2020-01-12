4ef142830892d38e159ea60cea71f4ca3340f58f
jcalcaben <jcalcaben@gmail.com>
2020-01-12 03:44:22 +0000

    jcalcaben published a site update

--- component---src-pages-404-js-df95ce8a393e1de559fa.js
+++ component---src-pages-404-js-df95ce8a393e1de559fa.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),s=n("POju");e.default=function(){return u.a.createElement(s.a,{title:"404: Not found",slug:"/404/"},u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-02253bf6f8e52adc437e.js.map
+//# sourceMappingURL=component---src-pages-404-js-df95ce8a393e1de559fa.js.map
--- component---src-pages-page-2-js-19c200b0c67ec9279b5f.js
+++ component---src-pages-page-2-js-19c200b0c67ec9279b5f.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[6],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("Wbzz"),l=a("POju");t.default=function(){return o.a.createElement(l.a,{title:"Page two",slug:"/page-2/"},o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-e9f9e17be66b6bca853d.js.map
+//# sourceMappingURL=component---src-pages-page-2-js-19c200b0c67ec9279b5f.js.map
