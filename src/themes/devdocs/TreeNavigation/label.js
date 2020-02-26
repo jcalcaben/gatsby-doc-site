afe8e6f884b340e7f16b92be5eeb027419a377ca
James Calcaben <jcalcaben+github@gmail.com>
2020-02-25 20:01:08 -0600

    Change tree nav toggle icons

--- src/themes/devdocs/TreeNavigation/label.js
+++ src/themes/devdocs/TreeNavigation/label.js
@@ -2,10 +2,17 @@ import React from "react"
 
 import classes from "./label.module.css"
 
+import ChevronUp from "@spectrum-css/icon/combined/ChevronUpSmall.svg"
+import ChevronDown from "@spectrum-css/icon/combined/ChevronDownSmall.svg"
+
 const Label = props => {
   const { url, label, open, toggleAction } = props
 
-  const toggleContent = open ? "-" : "+"
+  const toggleContent = open ? (
+    <ChevronUp className="spectrum-Icon spectrum-UIIcon-ChevronUpSmall" />
+  ) : (
+    <ChevronDown className="spectrum-Icon spectrum-UIIcon-ChevronUpSmall" />
+  )
 
   const toggleButton = toggleAction ? (
     <button className={classes.button} onClick={toggleAction}>
