fd95ee7e7ec70f440d1088221ddeb06521a6f651
James Calcaben <calcaben@adobe.com>
2020-04-17 14:06:11 -0500

    Fix whats new and GlobalVariable components

--- src/doc-components/GlobalVariable/globalVariable.js
+++ src/doc-components/GlobalVariable/globalVariable.js
@@ -1,9 +1,21 @@
 import React from "react"
 
-import data from "../../data/globalVariables.yml"
+import globalData from "../../data/globalVariables.yml"
+
+let localData = {}
+
+import("../../external/data/variables.yml")
+  .then(obj => {
+    localData = obj
+  })
+  .catch(err => {
+    console.log("Local data not found!")
+  })
 
 const GlobalVariable = props => {
   const { name, children } = props
+
+  const data = Object.assign(globalData, localData);
 
   const value = data[name] || data[children] || name || children
 
