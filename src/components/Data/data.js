fc6fc5fcaf50978fb24992a3503c8d96cd9b57a9
James Calcaben <calcaben@adobe.com>
2020-04-27 09:30:40 -0500

    Prevent sample page group from appearing

--- src/components/Data/data.js
+++ src/components/Data/data.js
@@ -7,7 +7,7 @@ const DataContext = React.createContext()
 const DataProvider = props => {
   const data = useStaticQuery(graphql`
     query {
-      pageGroups: allPageGroupsYaml {
+      pageGroups: allPageGroupsYaml(filter: { order: { gt: 0 } }) {
         nodes {
           order
           title
