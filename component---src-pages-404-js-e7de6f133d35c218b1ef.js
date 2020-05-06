74a2478ddf435a6bc2956a7672a7ffb8c6812dbe
jcalcaben <jcalcaben@gmail.com>
2020-05-06 16:07:10 +0000

    jcalcaben published a site update

--- component---src-pages-404-js-e7de6f133d35c218b1ef.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),s=n("POju");e.default=function(){return u.a.createElement(s.a,{title:"404: Not found",slug:"/404/"},u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e7de6f133d35c218b1ef.js.map
